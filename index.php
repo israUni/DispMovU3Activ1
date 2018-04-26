fbdc2af2fafc4a93e65e48f5e00b37e68790fcdb
Israel Eduardo Porfirio Moreno <israel.eduardopm@gmail.com>
2018-04-25 22:37:28 -0500

    Se instala la librería Guzzle y se actualiza el código en index.php

--- index.php
+++ index.php
@@ -2,19 +2,30 @@
 
 require 'vendor/autoload.php';
 
+use GuzzleHttp\Client;
+
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 $app = new \Slim\App;
 
-$app -> get('/clima', function (Request $request, Response $response, array $args) {
+$app -> get('/', function (Request $request, Response $response, array $args) {
 
-  $data = array (
-    'nombre' => 'Israel Eduardo Porfirio Moreno',
-    'cuenta' => '413104120'
-  );
+  // $data = array (
+  //   'nombre' => 'Israel Eduardo Porfirio Moreno',
+  //   'cuenta' => '413104120'
+  // );
 
-  return $response->withJson($data);
+  // return $response->withJson($data);
+
+  $client = new Client([
+    'base_uri' => 'http://api.openweathermap.org/data/2.5/',
+    // 'timeout'  => 2.0,
+  ]);
+
+  $response = $client -> request('GET', 'weather?id=3530597&APPID=11a2cc1182f522d6fd563dd9d72e111a');
+  
+  return $response;
 
 });
 
